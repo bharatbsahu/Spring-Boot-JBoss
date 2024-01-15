5110f31dc4e8237c5bf3190c040d5086c56d844b
bharatbsahu <smartbharat@gmail.com>
2024-01-16 00:49:54 +0530

    Spring Boot with JBoss

---
+++ src/main/java/com/jboss/spring/MyController.java
@@ -0,0 +1,15 @@
+package com.jboss.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		
+		return "Hello, Application is running on JBOSS EAP 7.4";
+	}
+
+}
